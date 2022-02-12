e13963451e13e0790dfedc884ec075ed0c1494ae
abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-02-12 09:32:16 +0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+fun main(){
+    var name= "Abinsinguza Dallen"
+    println(name)
+    var age ="20"
+    println(age)
+    var phonenumber = "0781698185"
+    println(phonenumber)
+    var weight ="60"
+    println(weight)
+    val iskenyan:Boolean =true
+    println(iskenyan) //output true
+    val notkenyan:Boolean =false
+    println(notkenyan) //output false
+
+}
